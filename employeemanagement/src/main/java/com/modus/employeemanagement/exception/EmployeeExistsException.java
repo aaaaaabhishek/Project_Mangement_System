c385d70932bc1260120e44fb96500141270f8c20
aaaaaabhishek <158762526+aaaaaabhishek@users.noreply.github.com>
2025-03-06 16:12:43 +0530

    Delete employeemanagement directory

--- employeemanagement/src/main/java/com/modus/employeemanagement/exception/EmployeeExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.modus.employeemanagement.exception;
-
-public class EmployeeExistsException extends RuntimeException{
-
-    public EmployeeExistsException(String message){
-        super(message);
-    }
-}
--- employeemanagement/src/main/java/com/modus/employeemanagement/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.modus.employeemanagement.repository;
-
-import com.modus.employeemanagement.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
